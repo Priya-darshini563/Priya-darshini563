6d1f5172c9aa048a78738a4d18806174be1c8e85
Priya-darshini563 <jaganpriya2262007@gmail.com>
2024-10-21 16:38:44 +0530

    All About Today.h

---
+++ All About Today.h
@@ -0,0 +1,6 @@
+Today I learned compton theory effect in my physics class
+I solved some problems in C programming
+I started with the concept of pointers in C
+I learned Heat treatment in chemistry 
+
+
